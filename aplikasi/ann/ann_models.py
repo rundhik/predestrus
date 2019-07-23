723aa6eb991df696d532b3ba0e251f28ab88a64c
rundhik <rundhik@yahoo.co.id>
2019-07-23 23:03:11 +0700

    Ann Eksperimen 2

--- aplikasi/ann/ann_models.py
+++ aplikasi/ann/ann_models.py
@@ -4,25 +4,74 @@ from .. import db
 from . import (
     np, pd, train_test_split, MinMaxScaler,
     accuracy_score, cohen_kappa_score, MLPClassifier,
-    roc_auc_score, confusion_matrix
+    roc_auc_score, confusion_matrix, resample
 )
 
 def getDataset(tabel='dataset'):
     df = pd.read_sql_table(tabel, db.engine)
     return df
-
-#call from aplikasi.ann import ann_models as ann
-# def tts(ts=0.2):
-    
-#     # x_train = Split(x_train)
-#     # x_test = Split(x_test)
-#     # y_train = Split(y_train)
-#     # y_test = Split(y_test)
-#     return x_train, x_test, y_train, y_test
-
-class Split:
+class Original:
     df = getDataset()
     df.columns = range(df.shape[1])
+    x =  df.loc[:,len(df.columns)-len(df.columns):len(df.columns)-2]
+    y = df.loc[:,len(df.columns)-1:len(df.columns)-1]
+    x_train, x_test, y_train, y_test = train_test_split(x,y, 
+                                                    test_size=.2, 
+                                                    random_state=1)
+    def xtrain(self, x_train):
+        return self.x_train
+    def xtest(self, x_test):
+        return self.x_test
+    def ytrain(self, y_train):
+        return self.y_train
+    def ytest(self, y_test):
+        return self.y_test
+
+class Oversampling:
+    df = getDataset()
+    df.columns = range(df.shape[1])
+    major = Original.y_train.loc[:,5].value_counts()[1]
+    minor = Original.y_train.loc[:,5].value_counts()[0]
+    df_majority = df[df.loc[:,5]==1]
+    df_minority = df[df.loc[:,5]==0]
+
+    # Upsample minority class (Oversampling)
+    df_minority_upsampled = resample(df_minority, 
+                                   replace=True, # sample with replacement
+                                   n_samples=major,  # to match majority class
+                                   random_state=123) # reproducible results
+    # Combine majority class with upsampled minority class
+    df = pd.concat([df_majority, df_minority_upsampled])
+
+    x =  df.loc[:,len(df.columns)-len(df.columns):len(df.columns)-2]
+    y = df.loc[:,len(df.columns)-1:len(df.columns)-1]
+    x_train, x_test, y_train, y_test = train_test_split(x,y, 
+                                                    test_size=.2, 
+                                                    random_state=1)
+    def xtrain(self, x_train):
+        return self.x_train
+    def xtest(self, x_test):
+        return self.x_test
+    def ytrain(self, y_train):
+        return self.y_train
+    def ytest(self, y_test):
+        return self.y_test
+
+class Undersampling:
+    df = getDataset()
+    df.columns = range(df.shape[1])
+    major = Original.y_train.loc[:,5].value_counts()[1]
+    minor = Original.y_train.loc[:,5].value_counts()[0]
+    df_majority = df[df.loc[:,5]==1]
+    df_minority = df[df.loc[:,5]==0]
+
+    # Downsample majority class (Undersampling)
+    df_majority_downsampled = resample(df_majority, 
+                                    replace=False,    # sample without replacement
+                                    n_samples=minor,     # to match minority class
+                                    random_state=123) # reproducible results
+    # Combine minority class with downsampled majority class
+    df = pd.concat([df_majority_downsampled, df_minority])
     x =  df.loc[:,len(df.columns)-len(df.columns):len(df.columns)-2]
     y = df.loc[:,len(df.columns)-1:len(df.columns)-1]
     x_train, x_test, y_train, y_test = train_test_split(x,y, 
